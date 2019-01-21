61815adf0d7e1fdd1cec983993c61c7e0be71be9
stefanFramework <cesar_cappetto@yahoo.com.ar>
2019-01-21 06:29:53 -0300

    Changing index configuration

--- web-application/index.php
+++ web-application/index.php
@@ -1,11 +1,11 @@
 <h1>Hello!</h1>
 Attempting to connect to database from php...</h4>
 <?php
-$host = 'localhost:5780';
+$host = 'database';
 $user = 'root';
 $pass = 'root';
 $db   = 'apache_docker';
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
